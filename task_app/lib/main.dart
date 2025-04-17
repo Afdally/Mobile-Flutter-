a70c1b9c984879e827ec5259d4d100ea84d168f1
Afdalgan <cikoh536@gmail.com>
2025-04-17 16:24:43 +0800

    Task app end

--- task_app/lib/main.dart
+++ task_app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:task_app/page/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter("hive_boxes");
   runApp(const MyApp());
 }
 
@@ -11,7 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Taskly App",
-      theme: ThemeData(primarySwatch: Colors.lightGreen),
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        useMaterial3: false, // Nonaktifkan Material 3
+      ),
       home: HomePage(),
     );
   }
--- task_app/lib/page/home_page.dart
+++ task_app/lib/page/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:task_app/models/task.dart';
 
 class HomePage extends StatefulWidget {
   HomePage();
@@ -11,46 +13,111 @@ class HomePage extends StatefulWidget {
 class _HomePgeState extends State<HomePage> {
   late double _deviceHeight, _deviceWidth;
 
+  String? _newTaskContent;
+
+  Box? _box;
+
   _HomePgeState();
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+    print("input valude: $_newTaskContent");
 
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: _deviceHeight * 0.15,
         title: Text("Taskly!", style: TextStyle(fontSize: 25)),
       ),
-      body: _taskList(),
+      body: _tasksView(),
       floatingActionButton: _addTaskButton(),
     );
   }
 
+  Widget _tasksView() {
+    return FutureBuilder(
+      future: Hive.openBox('task'),
+      builder: (BuildContext _context, AsyncSnapshot _snapshot) {
+        if (_snapshot.hasData) {
+          _box = _snapshot.data;
+          return _taskList();
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
   Widget _taskList() {
-    return ListView(
-      children: [
-        ListTile(
-          title: const Text(
-            "belajar",
-            style: TextStyle(decoration: TextDecoration.lineThrough),
+    List tasks = _box!.values.toList();
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (BuildContext _context, int _index) {
+        var task = Task.fromMap(tasks[_index]);
+        return ListTile(
+          title: Text(
+            task.content,
+            style: TextStyle(
+              decoration: task.done ? TextDecoration.lineThrough : null,
+            ),
           ),
-          subtitle: Text(DateTime.now().toString()),
-          trailing: Icon(Icons.check_box_outlined, color: Colors.lightGreen),
-        ),
-        ListTile(
-          title: const Text(
-            "Ngoding",
-            style: TextStyle(decoration: TextDecoration.lineThrough),
+          subtitle: Text(task.timestamp.toString()),
+          trailing: Icon(
+            task.done
+                ? Icons.check_box_outlined
+                : Icons.check_box_outline_blank,
+            color: Colors.lightGreen,
           ),
-          subtitle: Text(DateTime.now().toString()),
-          trailing: Icon(Icons.check_box_outlined, color: Colors.lightGreen),
-        ),
-      ],
+          onTap: () {
+            task.done = !task.done;
+            _box!.putAt(_index, task.toMap());
+            setState(() {});
+          },
+          onLongPress: () {
+            _box!.deleteAt(_index);
+            setState(() {});
+          },
+        );
+      },
     );
   }
 
   Widget _addTaskButton() {
-    return FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add));
+    return FloatingActionButton(
+      onPressed: _displayTaskPopup,
+      child: const Icon(Icons.add),
+    );
+  }
+
+  void _displayTaskPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext _context) {
+        return AlertDialog(
+          title: const Text("Add New Task!"),
+          content: TextField(
+            onSubmitted: (_) {
+              if (_newTaskContent != null) {
+                var _task = Task(
+                  content: _newTaskContent!,
+                  timestamp: DateTime.now(),
+                  done: false,
+                );
+                _box!.add(_task.toMap());
+              }
+              setState(() {
+                _newTaskContent = null;
+                Navigator.pop(context);
+              });
+            },
+            onChanged: (_value) {
+              setState(() {
+                _newTaskContent = _value;
+              });
+            },
+          ),
+        );
+      },
+    );
   }
 }
